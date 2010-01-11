e891f4e78c861d1cc96ec2847ec45aee2ffa5781
inbird81 <inbird81@b2ad4bee-f8dc-11de-a40b-ad22939d179f>
2010-01-11 09:18:55 +0000

    v0.1.3

--- trunk/Sheet/Character/Spells.cs
+++ trunk/Sheet/Character/Spells.cs
@@ -51,8 +51,19 @@ namespace Sheet
             // 캐릭터에 없는 클래스면 0을 반환
             if(classLevel < 1) return 0;
 
+            // 기본 주문 갯수 얻기
             count = casterClass.SpellPerDay[classLevel, spellLevel];
+            if (count < 0) return 0; // -1일 경우 해당주문 사용불가.
 
+            // 능력치에 의한 추가 주문 갯수 얻기.
+            if (spellLevel > 0) // 0레벨은 능력치에 의한 추가주문갯수가 없음
+            {
+                int bonus = GetAbilityBonus(casterClass.RelatedAbility);
+                if (bonus == spellLevel) count += 1; // 0으로 나눌 수 없으므로 예외처리해줌.
+                else if(bonus > spellLevel) count += (bonus - spellLevel) / 4 + 1;
+            }
+
+            // 이펙트에 따른 주문 개수 보너스 얻기.
             count += GetEffectValue("LEVEL_" + spellLevel + "_SPELL_COUNT_BOUNS");
 
             return count;
@@ -62,6 +73,15 @@ namespace Sheet
         {
             int dc = 10;
             // 연관능력치 정보 얻기
+
+            // 클래스 얻어오기
+            ClassInfo casterClass = DataManager.Instance.GetClass(m_selectedSpellCastingClass);
+
+            // 데이터에 없는 클래스면 DC 0을 반환.
+            if (casterClass == null) return 0;
+
+            // 연관능력치의 보너스를 주문의 DC에 추가.
+            dc += GetAbilityBonus(casterClass.RelatedAbility);
 
             return dc;
         }
